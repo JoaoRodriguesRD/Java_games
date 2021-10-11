af352737de9fd65abb722ec19294d35cd196fb63
João Rodrigues De Melo Neto <j_neto_rodrigues@hotmail.com>
2021-10-11 09:56:32 -0300

    A base para trabalhar com gráficos em Java

---
+++ GraficosSimplificados/src/com/jrodrigues/main/Game.java
@@ -0,0 +1,222 @@
+package com.jrodrigues.main;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferStrategy;
+
+import javax.swing.JFrame;
+
+
+
+public class Game extends Canvas implements Runnable, KeyListener, MouseListener, MouseMotionListener{
+	
+	public JFrame frame;
+	private Thread thread;
+	private boolean isRunning;
+	
+	public static int frames = 0;
+	
+	public static final int WIDTH = 320;
+	public static final int HEIGHT = 230;
+	public static final int SCALE = 2;
+	
+	public Game() {
+		addKeyListener(this); // adiciona evento de KeyListener nesta classe
+
+		addMouseListener(this);
+
+		addMouseMotionListener(this);
+		
+		setPreferredSize(new Dimension(Toolkit.getDefaultToolkit().getScreenSize()));
+		initFrame();
+
+	}
+	
+
+
+	public void initFrame() {
+		frame = new JFrame("Titulo");
+		frame.add(this);
+		frame.setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
+		//frame.setUndecorated(true); //tela cheia
+		frame.pack();
+		frame.setResizable(false);
+		// iniciar no centro
+		frame.setLocationRelativeTo(null);
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		frame.setVisible(true);
+	}
+
+
+
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.start();
+	}
+	
+	public synchronized void start() {
+		thread = new Thread(this);
+		isRunning = true;
+		thread.start();
+	}
+
+	
+
+
+
+	@Override
+	public void run() {
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+
+		double timer = System.currentTimeMillis();
+		while (isRunning) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+
+			if (delta >= 1) {
+				tick();
+				render();
+
+				frames++;
+				delta--;
+			}
+
+			if (System.currentTimeMillis() - timer >= 1000) {
+				System.out.println("FPS:" + frames);
+
+				frames = 0;
+				timer += 1000;
+			}
+		}
+
+		stop();
+	}
+
+
+
+	private void stop() {
+		isRunning = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+
+			e.printStackTrace();
+		}
+	}
+
+
+
+	private void render() {
+		// TODO Auto-generated method stub
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		Graphics g = bs.getDrawGraphics();
+		g.setColor(Color.blue);
+		g.fillRect(0, 0, WIDTH*SCALE, HEIGHT*SCALE);
+		
+		
+		
+		g.dispose();
+		bs.show();
+	}
+
+
+
+	private void tick() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
